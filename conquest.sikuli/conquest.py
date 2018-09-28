ddebe931489865ab81561195dd842ac739ff1431
Yurii Ivanov <yivanov00@gmail.com>
2018-09-28 15:33:49 +0300

    Add conquest of sword automation script.

---
+++ conquest.sikuli/conquest.py
@@ -0,0 +1,48 @@
+def waitAny(images, time):
+    for t in range(time):
+        image = findBestList(images)
+        if image is not None:
+            return image
+        else:
+            sleep(1)
+    else:
+        return None
+
+
+def doCleanup():
+    click("1538132386528.png")
+    sleep(1)
+    
+    i = 0
+    while exists(Pattern("1533754575036.png").similar(0.80).targetOffset(0,122)) and i < 3:
+        click()
+        click("1533842472749.png")
+        click(waitAny(["1533489783564.png", "1538135930753.png"], 180))
+        i += 1
+
+
+def doConquest(conquest, difficulty):
+    click(conquest)
+    click(difficulty)
+    if exists("1533574629012-2.png"):
+        click()
+    
+    doCleanup()
+    
+    click("1533153247035.png")
+    click(Pattern("1538136983838.png").targetOffset(15,-1))
+
+
+hover("1533067526354.png")
+hover("1534061342651.png")
+click("1533147124645.png")
+
+conquests = [
+        (Pattern("1533147154734.png").targetOffset(0,140), Pattern("1538132303896.png").similar(0.85)), 
+        (Pattern("1538132636055.png").targetOffset(8,133), Pattern("1538132680592.png").similar(0.80)),
+        (Pattern("1538132807630.png").targetOffset(0,134), Pattern("1538132680592.png").similar(0.80))]
+
+for conquest in conquests:
+    doConquest(*conquest)
+
+click(Pattern("1538137875037.png").targetOffset(15,0))
